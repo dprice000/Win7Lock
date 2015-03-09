e474d29c52361977f493963ba5cffadbab49b16f
David Price <Davidp809@gmail.com>
2015-03-08 23:19:32 -0400

    Updated readme and added basic functionality

---
+++ Win7Lock/README.md
@@ -0,0 +1,11 @@
+# Win7Lock
+A C# library for locking setting up a windows kiosk  
+
+This application was created to mimic the third party WinLock library. 
+The WinLock library was originally created for Windows XP and is now deprecated.
+This library has been created with a mixture of the windows API and the Group Policy registries
+to lock down windows. 
+
+#ResetWinExplorer()
+A function called ResetWinExplorer() has been created as quick way to restart Windows Explorer. This call is required after
+most Win7Lock function calls. Those calls that do not require this call have been noted in the remarks field. 
--- Win7Lock/Winlock.cs
+++ Win7Lock/Winlock.cs
@@ -9,25 +9,124 @@ namespace Win7Lock
 {
     public static class Winlock
     {
-        const String explorerPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer";
-        const String systemPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
-        static Microsoft.Win32.RegistryKey explorerKey = Registry.CurrentUser.CreateSubKey(explorerPath);
+        const String mExplorerPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer";
+        const String mSystemPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+        static Microsoft.Win32.RegistryKey mExplorerKey = Registry.CurrentUser.CreateSubKey(mExplorerPath);
+        static Microsoft.Win32.RegistryKey mSystemKey = Registry.CurrentUser.CreateSubKey(mSystemPath);
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 1;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr FindWindowExW(IntPtr parentHWnd, IntPtr childAfterHWnd, IntPtr className, string windowTitle);
+
+        /// <summary>
+        /// Uses the Windows API to re-enable the taskbar
+        /// </summary>
+        /// <remarks>Calling ResetWinExplorer() will reset this setting</remarks>
+        public static void ShowTaskBar()
+        {
+            ShowWindow(FindWindow("Shell_TrayWnd", ""), SW_SHOW);
+        }
+
+        /// <summary>
+        /// Uses the Windows API to hide the taskbar
+        /// </summary>
+        /// <remarks>Calling ResetWinExplorer() will reset this setting</remarks>
+        public static void HideTaskBar()
+        {
+            ShowWindow(FindWindow("Shell_TrayWnd", ""), SW_HIDE);
+        }
+
+        /// <summary>
+        /// Starts a third party process that disables Winkeys.
+        /// </summary>
+        /// <remarks>
+        /// Does not require ResetWinExplorer()
+        /// </remarks>
+        public static void DisableWinKeys()
+        {
+            System.Diagnostics.Process.Start("NoWinKey.exe");
+        }
+
+        /// <summary>
+        /// Kills the third party process that disables WinKeys
+        /// </summary>
+        /// <remarks>
+        /// Does not require ResetWinExplorer()
+        /// </remarks>
+        public static void EnableWinKeys()
+        {
+            System.Diagnostics.Process[] noAltTabProc = System.Diagnostics.Process.GetProcessesByName("NoWinKey");
+
+            foreach (System.Diagnostics.Process proc in noAltTabProc)
+            {
+                proc.Kill();
+            }
+        }
+
+        /// <summary>
+        /// Kills the third party process that disables Alt+Tab
+        /// </summary>
+        /// <remarks>
+        /// Does not require ResetWinExplorer()
+        /// </remarks>
+        public static void EnableAltTab()
+        {
+            System.Diagnostics.Process.Start("NoAltTab.exe");
+        }
+
+        /// <summary>
+        /// Starts a third party process that disables Alt+Tab
+        /// </summary>
+        /// <remarks>
+        /// Does not require ResetWinExplorer()
+        /// </remarks>
+        public static void DisableAltTab()
+        {
+            System.Diagnostics.Process[] noAltTabProc = System.Diagnostics.Process.GetProcessesByName("NoAltTab");
+
+            foreach (System.Diagnostics.Process proc in noAltTabProc)
+            {
+                proc.Kill();
+            }
+        }
 
         /// <summary>
         /// Re-enables all of the icons and desktop settings.
         /// </summary>
         public static void EnableDesktop()
         {
-            explorerKey.SetValue("NoDesktop", 0x0, RegistryValueKind.DWord);
+            mExplorerKey.SetValue("NoDesktop", 0x0, RegistryValueKind.DWord);
         }
-
 
         /// <summary>
         /// Hides all icons and prevents any interaction with the desktop.
         /// </summary>
         public static void DisableDesktop()
         {
-            explorerKey.SetValue("NoDesktop", 0x1, RegistryValueKind.DWord);
+            mExplorerKey.SetValue("NoDesktop", 0x1, RegistryValueKind.DWord);
+        }
+
+        /// <summary>
+        /// Prevents user from accessing task manager
+        /// </summary>
+        public static void DisableTaskMgr()
+        {
+            mSystemKey.SetValue("DisableTaskMgr", 0x1, RegistryValueKind.DWord);
+        }
+
+        /// <summary>
+        /// Enables the task manager
+        /// </summary>
+        public static void EnableTaskMgr()
+        {
+            mSystemKey.SetValue("DisableTaskMgr", 0x0, RegistryValueKind.DWord);
         }
 
         /// <summary>
@@ -35,7 +134,7 @@ namespace Win7Lock
         /// </summary>
         public static void HideClock()
         {
-            explorerKey.SetValue("HideClock", 0x1, RegistryValueKind.DWord);
+            mExplorerKey.SetValue("HideClock", 0x1, RegistryValueKind.DWord);
         }
 
         /// <summary>
@@ -46,7 +145,7 @@ namespace Win7Lock
         /// </remarks>
         public static void ShowClock()
         {
-            explorerKey.SetValue("HideClock", 0x0, RegistryValueKind.DWord);
+            mExplorerKey.SetValue("HideClock", 0x0, RegistryValueKind.DWord);
         }
 
         /// <summary>
@@ -56,6 +155,16 @@ namespace Win7Lock
         /// <param name="delay">Number of senconds between when win explorer is closed and re-opened</param>
         public static void ResetWinExplorer(int delay)
         {
+            var explorerProcs = System.Diagnostics.Process.GetProcessesByName("explorer");
+
+            foreach(var process in explorerProcs)
+            {
+                process.Kill();
+            }
+
+            System.Threading.Thread.Sleep(delay * 1000);
+
+            System.Diagnostics.Process.Start("explorer.exe");
         }
     }
 }
